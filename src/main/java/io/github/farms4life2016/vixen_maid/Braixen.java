1b472cf2f1e5bda26aa81f676017b6d9713154d3
farms4life2016 <farms4life2016@gmail.com>
2026-05-24 18:58:50 -0400

    make braixen immune to berrybush damage
    unfortunately she's still slowed down by bushes,
    can't fight vanilla hardcode :P

--- src/main/java/io/github/farms4life2016/vixen_maid/Braixen.java
+++ src/main/java/io/github/farms4life2016/vixen_maid/Braixen.java
@@ -1,12 +1,26 @@
 package io.github.farms4life2016.vixen_maid;
 
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Fox;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 // for now just extend vanilla Fox AI, we can customize later...
 public class Braixen extends Fox {
+    private final List<ResourceKey<DamageType>> immuneTo = List.of(DamageTypes.SWEET_BERRY_BUSH);
+    // if we want fire immunity, we can add LAVA, IN_FIRE, CAMPFIRE, HOT_FLOOR, etc.
+
     public Braixen(EntityType<? extends Fox> entityType, Level level) {
         super(entityType, level);
+    }
+
+    @Override
+    public boolean isInvulnerableTo(DamageSource source) {
+        return immuneTo.stream().anyMatch(source::is) || super.isInvulnerableTo(source);
     }
 }
